8bdfc8f0d805a4721cbabea4fa2e99c025b3a23c
Kelvin-Squishy <obengappiah98@gmail.com>
2022-09-21 21:04:30 +0800

    useState practice

---
+++ data.js
@@ -0,0 +1,37 @@
+const employees = [
+  {
+    id: 1,
+    name: 'Derek Eskimo',
+    image: 'images/channel-1.jpeg',
+    salary: '$500.00',
+  },
+  {
+    id: 2,
+    name: 'Kennedy Pablo',
+    image: 'images/channel-2.jpeg',
+    salary: '$100.50',
+  },
+
+  {
+    id: 3,
+    name: 'Alpha Dennis',
+    image: 'images/channel-3.jpeg',
+    salary: '$1200.00',
+  },
+
+  {
+    id: 4,
+    name: 'Kelvin Squishy',
+    image: 'images/channel-6.jpeg',
+    salary: '$ 1500.00',
+  },
+
+  {
+    id: 5,
+    name: 'Appiah Joseph',
+    image: 'images/channel-5.jpeg',
+    salary: '$ 800.00',
+  },
+];
+
+export default employees;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,72 @@
 import styles from '../styles/Home.module.css';
+
 import { useState } from 'react';
 
+import Employees from '../data';
+
+import Image from 'next/image';
+
 export default function Home() {
-  return <h1>Hey</h1>;
+  const [data, setData] = useState(Employees);
+
+  const remEmployee = (id) => {
+    setData(data.filter((employee) => employee.id !== id));
+  };
+
+  const remAllEmployees = () => {
+    setData([]);
+  };
+
+  return (
+    <section className={styles.card}>
+      <h1 style={{ alignSelf: 'center' }}>Employee List</h1>
+
+      {data.map((employee) => {
+        const { name, id, image, salary } = employee;
+
+        return (
+          <li
+            key={id}
+            style={{
+              border: '1px solid rgb(180, 174, 174)',
+              display: 'flex',
+              alignItems: 'center',
+              margin: '1rem',
+            }}
+          >
+            <div style={{ margin: '0.5rem' }}>
+              <Image
+                style={{ borderRadius: '50px' }}
+                src={'/' + image}
+                width={100}
+                height={100}
+                alt={name}
+              />
+            </div>
+            <div style={{ flex: '4rem' }}>
+              <h1>{name}</h1>
+              <p>{salary}</p>
+            </div>
+            <button
+              style={{
+                border: 'none',
+                color: '#fff',
+                backgroundColor: 'red',
+                fontWeight: 'bold',
+                padding: '0.7rem',
+                marginRight: '2rem',
+              }}
+              onClick={() => remEmployee(id)}
+            >
+              remove
+            </button>
+          </li>
+        );
+      })}
+
+      <button className={styles.btn} onClick={remAllEmployees}>
+        Remove All
+      </button>
+    </section>
+  );
 }
